56b21aaa6eb1f5408066cb8dd614171cf3d5936a
Francisco Figueiredo <francisco-abf@hotmail.com>
2025-05-03 18:56:52 +0100

    js fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
+import "bootstrap"
 import "controllers"
 import "turbo-rails"
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,11 @@ pin "application"
 pin "turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "bootstrap/dist/js/bootstrap.bundle.min.js", to: "bootstrap.min.js", preload: true
+pin "bootstrap/dist/css/bootstrap.min.css", to: "bootstrap.min.css", preload: true
+pin "bootstrap/dist/js/bootstrap.bundle.min.js", to: "bootstrap.min.js", preload: true
+pin "bootstrap/dist/css/bootstrap.min.css", to: "bootstrap.min.css", preload: true
+
 
 pin_all_from "app/javascript/controllers", under: "controllers"
