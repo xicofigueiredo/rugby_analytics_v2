b12b103a8cd9c7efa6f63594cd410c744ca6daf5
Francisco Figueiredo <francisco-abf@hotmail.com>
2025-05-04 20:47:23 +0100

    shirst fit the field

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,21 +1,21 @@
 module MatchesHelper
   def get_home_position(num)
     positions = {
-      15 => 'left: 50%; top: 5%;',   # Fullback
-      14 => 'left: 70%; top: 5%;',  # Right Wing
-      13 => 'left: 62%; top: 22%;',  # Outside Center
-      12 => 'left: 37%; top: 25%;',  # Inside Center
-      11 => 'left: 30%; top: 5%;',  # Left Wing
-      10 => 'left: 60%; top: 42%;',  # Fly-half
-      9  => 'left: 40%; top: 45%;',  # Scrum-half
-      8  => 'left: 50%; top: 60%;',  # Number 8
-      7  => 'left: 70%; top: 60%;',  # Openside Flanker
-      6  => 'left: 30%; top: 60%;',  # Blindside Flanker
-      5  => 'left: 60%; top: 75%;',  # Lock
-      4  => 'left: 40%; top: 75%;',  # Lock
-      3  => 'left: 70%; top: 90%;',  # Tighthead Prop
-      2  => 'left: 50%; top: 90%;',  # Hooker
-      1  => 'left: 30%; top: 90%;'   # Loosehead Prop
+      15 => 'left: 45%; top: 22%;',   # Fullback
+      14 => 'left: 89%; top: 25%;',  # Right Wing
+      13 => 'left: 77%; top: 30%;',  # Outside Center
+      12 => 'left: 65%; top: 35%;',  # Inside Center
+      11 => 'left: 15%; top: 25%;',  # Left Wing
+      10 => 'left: 53%; top: 40%;',  # Fly-half
+      9  => 'left: 31%; top: 45%;',  # Scrum-half
+      8  => 'left: 31%; top: 60%;',  # Number 8
+      7  => 'left: 47%; top: 60%;',  # Openside Flanker
+      6  => 'left: 15%; top: 60%;',  # Blindside Flanker
+      5  => 'left: 39%; top: 70%;',  # Lock
+      4  => 'left: 23%; top: 70%;',  # Lock
+      3  => 'left: 47%; top: 80%;',  # Tighthead Prop
+      2  => 'left: 31%; top: 80%;',  # Hooker
+      1  => 'left: 15%; top: 80%;'   # Loosehead Prop
     }
     positions[num] || ''
   end
