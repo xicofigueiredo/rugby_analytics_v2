66a9430f6129654b3f8fe143926a6c9000a72149
Francisco Figueiredo <francisco-abf@hotmail.com>
2025-10-23 03:40:18 +0800

    Fix JavaScript errors preventing dropdown functionality: add null checks and Turbo Rails import

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application"
 pin "turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
